288fcefaaffe598172f2a8d5b7ad9d9f7392797a
Raquel Azcue <rvazcue@gmail.com>
2018-08-02 14:34:10 -0500

    setting up the db and creating the items inside the db

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burgers_db ;
+USE burgers_db
+CREATE TABLE burgers (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BIT DEFAULT 0
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+USE burgers_db ; 
+
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ('cheeseburger', 0),('veggieburger', 0), ('venison-burger', 0),
