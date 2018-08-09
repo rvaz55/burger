35d4fbf65b751ee46333a7dcd1a793cb6c2fe57c
Raquel Azcue <rvazcue@gmail.com>
2018-08-08 22:12:56 -0500

    setting up heroku / status: in progress

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 let mysql = require("mysql")
+let connection
 
 
 //Creating an instance of 'mysql.createConnect({ code to access a DB})'
 //then storing it in a VAR/LET named 'connection'
 
-let connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Athlon64!!",
     database: "burgers_db"
-});
+                            });
+}
+
 
 //Caliing the '.connection' method on the the Let connection 
 connection.connect(function(err){
