bf2fe562892af8ff6696a81ffc0aaceda92dd0f6
Raquel Azcue <rvazcue@gmail.com>
2018-08-09 11:14:01 -0500

    sending upfated files

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
+DROP DATABASE burgers_db ;
 CREATE DATABASE burgers_db ;
 
-
+USE burgers_db ; 
 CREATE TABLE burgers (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
